9fa5c91f061bae0ffa40ae79e45b4db7e3bd96df
MacBook Air <liebespaar93@gmail.com>
2022-08-06 01:55:57 +0900

    2022/08/06 learn include error fix

--- mlx_learn/step_3_animation/src_loop/ft_loop.h
+++ mlx_learn/step_3_animation/src_loop/ft_loop.h
@@ -6,12 +6,14 @@
 /*   By: kyoulee <kyoulee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/05 22:12:07 by kyoulee           #+#    #+#             */
-/*   Updated: 2022/08/05 22:32:19 by kyoulee          ###   ########.fr       */
+/*   Updated: 2022/08/05 22:56:20 by kyoulee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_LOOP_H
 # define FT_LOOP_H
+
+# include <ft_param.h>
 
 void	ft_key_update(t_screan *screan, t_keyboard *keyboard);
 void	ft_mouse_update(t_screan *screan, t_mouse *mouse);
